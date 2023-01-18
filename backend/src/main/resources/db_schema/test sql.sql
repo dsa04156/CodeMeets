bdfa1d66cdddee3fe4b93cacbf54a1eaf76db0e1
aeoragy <aeoragy@gmail.com>
2023-01-18 13:46:58 +0900

    [BE] docs : SQL Test
    mybatis 적용 전 sql 테스트하는 파일

---
+++ backend/src/main/resources/db_schema/test sql.sql
@@ -0,0 +1,41 @@
+use s08p11d109;
+
+select *
+from user;
+
+select *
+from user_info;
+
+select *
+from user natural join user_info;
+
+delete from user where user_pk = 3;
+delete from user_info where user_pk = 3;
+
+ALTER TABLE user_info AUTO_INCREMENT = 2;
+ALTER TABLE user AUTO_INCREMENT = 2;
+
+select *
+from user_info
+where user_pk = 1;
+
+select user_pk
+from user
+where user_id = 'test01';
+
+
+ALTER TABLE `s08p11d109`.`user` 
+CHANGE `token` `token` VARCHAR(256);
+
+
+
+START TRANSACTION;
+	INSERT INTO `s08p11d109`.`user`(user_id, `password`, token, user_active) 
+	VALUES('test03', '1234', 'token', 1);
+    
+	INSERT INTO `s08p11d109`.`user_info` (user_name, nickname, tel, email, profile_photo, profile_public, user_pk) 
+    VALUES('테스트03', '테스트닉네임03', '010-0303-0303', '이메일03@주소', '프로필사진', 1, (SELECT user_pk 
+    FROM user 
+    WHERE user_id = 'test03'));
+    
+COMMIT;
