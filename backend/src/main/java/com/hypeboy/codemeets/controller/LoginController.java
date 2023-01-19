c2f3d38c49fb28fdca428101866a18690abd5717
aeoragy <aeoragy@gmail.com>
2023-01-19 13:19:47 +0900

    [BE] feat : Referesh Toekn
    refresh-token 과정 추가

--- backend/src/main/java/com/hypeboy/codemeets/controller/LoginController.java
+++ backend/src/main/java/com/hypeboy/codemeets/controller/LoginController.java
@@ -3,11 +3,15 @@ package com.hypeboy.codemeets.controller;
 import java.util.HashMap;
 import java.util.Map;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -65,6 +69,55 @@ public class LoginController {
 			logger.info("토큰 사용 불가능");
 			resultMap.put("message", e.getMessage());
 			status = HttpStatus.INTERNAL_SERVER_ERROR;
+		}
+		return new ResponseEntity<Map<String, Object>>(resultMap, status);
+	}
+	
+	@Operation(summary = "Get Login UserInfo", description = "로그인 상태의 유저 정보 획득 API."
+			+ " \n\n 아이디와 헤더에 담긴 토큰으로 검사")
+	@GetMapping("/{id}")
+	public ResponseEntity<Map<String, Object>> getInfo(@PathVariable("id") String id, HttpServletRequest request) {
+		Map<String, Object> resultMap = new HashMap<String, Object>();
+		HttpStatus status = HttpStatus.UNAUTHORIZED;
+		
+		if (jwtTokenProvider.validateToken(request.getHeader("access-token"))) {
+			logger.info("사용가능한 토큰입니다");
+			try {
+				UserDto userDto = loginService.getUserInfo(id);
+				resultMap.put("userInfo", userDto);
+				resultMap.put("message", SUCCESS);
+				status = HttpStatus.ACCEPTED;
+			} catch (Exception e) {
+				logger.info("사용자 정보 조회 실패" + " " + e);
+				resultMap.put("message", FAIL);
+				status = HttpStatus.UNAUTHORIZED;
+			} 
+		} else {
+			logger.info("사용 불가능한 토큰입니다");
+			resultMap.put("message", FAIL);
+			status = HttpStatus.INTERNAL_SERVER_ERROR;
+		}
+		return new ResponseEntity<Map<String,Object>>(resultMap, status);
+	}
+	
+	@Operation(summary = "Get Access-Token", description = "유저의 refresh-token 사용하여 access-token 재발급")
+	@PostMapping("/refresh")
+	public ResponseEntity<?> refreshToken(@RequestBody UserDto userDto, HttpServletRequest request) throws Exception {
+		Map<String, Object> resultMap = new HashMap<String, Object>();
+		HttpStatus status = HttpStatus.ACCEPTED;
+		String token = request.getHeader("refresh-token");
+		
+		if (jwtTokenProvider.validateToken(token)) {
+			if (token.equals(loginService.getRefreshToken(userDto.getUserId()))) {
+				String accessToken = jwtTokenProvider.createAccessToken("id", userDto.getUserId());
+				resultMap.put("access-token", accessToken);
+				resultMap.put("message", SUCCESS);
+				status = HttpStatus.ACCEPTED;
+				logger.info("액세스 토큰 재발급 완료");
+			}
+		} else {
+			logger.info("리프레쉬 토큰 사용 불가");
+			status = HttpStatus.UNAUTHORIZED;
 		}
 		return new ResponseEntity<Map<String, Object>>(resultMap, status);
 	}
--- backend/src/main/java/com/hypeboy/codemeets/model/service/LoginService.java
+++ backend/src/main/java/com/hypeboy/codemeets/model/service/LoginService.java
@@ -5,5 +5,9 @@ import com.hypeboy.codemeets.model.dto.UserDto;
 
 public interface LoginService {
 	public UserDto login(LoginDto loginDto) throws Exception;
+	public UserDto getUserInfo(String id) throws Exception;
+	
+	// token
 	public void saveRefreshToken(String userid, String refreshToken) throws Exception;
+	public Object getRefreshToken(String userId) throws Exception;
 }
