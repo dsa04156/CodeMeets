27c1b6cf97f8b5fcff33bc692060320aa59cf178
SSAFY <dsa04156@naver.com>
2023-01-25 15:57:19 +0900

    230125 group

--- backend/src/main/java/com/hypeboy/codemeets/model/service/GroupService.java
+++ backend/src/main/java/com/hypeboy/codemeets/model/service/GroupService.java
@@ -4,6 +4,7 @@ import java.sql.SQLException;
 import java.util.List;
 
 import com.hypeboy.codemeets.model.dto.GroupDto;
+import com.hypeboy.codemeets.model.dto.GroupUserDto;
 import com.hypeboy.codemeets.model.dto.UserDto;
 
 public interface GroupService {
@@ -11,5 +12,5 @@ public interface GroupService {
 
 	List<UserDto> groupMemberList(int groupPk) throws SQLException;
 
-	void groupJoin(int groupPk,int userPk) throws SQLException;
+	int groupJoin(GroupUserDto guDto) throws SQLException;
 }
