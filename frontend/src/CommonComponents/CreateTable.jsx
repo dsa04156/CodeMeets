c541f05efcf8b6ad85e7cf61512a5b332d2c2227
LEESEOKKI <seokki0328@naver.com>
2023-02-01 16:50:49 +0900

    2021 mypage

--- frontend/src/CommonComponents/CreateTable.jsx
+++ frontend/src/CommonComponents/CreateTable.jsx
@@ -1,13 +1,12 @@
 import React from 'react'
 import { useTable, useBlockLayout } from 'react-table'
-import { useRecoilState } from 'recoil'
-import { TableData } from '../Store'
+import { useState } from 'react'
+import styled from 'styled-components';
+import { NavLink } from "react-router-dom";
+
 
 function CreateTable({ columns, data, TableNavHandler}) {
   // Use the state and functions returned from useTable to build your UI
-  const [tableData, setTableData] = useRecoilState(TableData)
-
-
 
   const defaultColumn = React.useMemo(
     () => ({
@@ -33,14 +32,15 @@ function CreateTable({ columns, data, TableNavHandler}) {
 
   // Render the UI for your table
   return (
+    <Styles>
     <div {...getTableProps()} className="table">
       <div>
         {headerGroups.map(headerGroup => (
           <div {...headerGroup.getHeaderGroupProps()} className="tr">
             {headerGroup.headers.map(column => (
-              <div {...column.getHeaderProps()} className="th">
+              <HeadSize {...column.getHeaderProps()} className="th">
                 {column.render('Header')}
-              </div>
+              </HeadSize>
             ))}
           </div>
         ))}
@@ -55,14 +55,13 @@ function CreateTable({ columns, data, TableNavHandler}) {
               className="tr" 
               onClick={() => {TableNavHandler(row);
                 console.log(row.original)
-                setTableData(row.original)
               
               }}>
                 {row.cells.map(cell => {
                   return (
-                    <div {...cell.getCellProps()} className="td">
+                    <NavBarStyle {...cell.getCellProps()} className="td">
                       {cell.render('Cell')}
-                    </div>
+                    </NavBarStyle>
                   )
                 })}
               </div>
@@ -70,6 +69,52 @@ function CreateTable({ columns, data, TableNavHandler}) {
         )}
       </div>
     </div>
+    </Styles>
   )
 }
 export default CreateTable;
+
+const Styles = styled.div`
+  text-align: center;
+  line-height: 2.5;
+  padding: 1rem;
+  padding-top: 0px;
+  table {
+    border-spacing: 0;
+    border: 1px solid black;
+    font-size: 5em;
+    tr {
+      :last-child {
+        td {
+          border-bottom: 0;
+        }
+      }
+    }
+    th,
+    td {
+      margin: 0;
+      padding: 0.5rem;
+      border-bottom: 1px solid black;
+      border-right: 1px solid black;
+      :last-child {
+        border-right: 0;
+      }
+    }
+  }
+`;
+
+const NavBarStyle = styled(NavLink)`
+  color: black;
+  outline: invert;
+  &:link {
+    transition : 0.5s;
+    text-decoration: none;
+  }
+  &:hover {
+    color: #10f14c;
+  }
+`;
+
+const HeadSize = styled.div`
+  font-size: large;
+`;
--- frontend/src/MyPage/MyPagePages/MyPageQuestionList.jsx
+++ frontend/src/MyPage/MyPagePages/MyPageQuestionList.jsx
@@ -1,9 +1,84 @@
+import { APIroot } from '../../Store';
+import { useRecoilState, useRecoilValue } from 'recoil';
+import React, { useState, useEffect } from 'react';
+import { useNavigate } from "react-router-dom";
+import axios from 'axios';
+import styled from 'styled-components';
+import CreateTable from '../../CommonComponents/CreateTable';
+
 const MyPageQuestionList = () => {
+    const [questionRecord, setQuestionRecord] = useState([]);
+    const API = useRecoilValue(APIroot);
+    const navigate = useNavigate();
+    
+    const TableNavHandler = () => {
+        navigate('/MyPageQuestionListDetail');
+    }
+    
+    const data = React.useMemo(() => questionRecord, [questionRecord]);
+
+    const columns = React.useMemo(
+        () => [
+            { Header: '번호', accessor: 'conferencePk', width: 90},
+            { Header: '질문 내용', accessor: 'conferenceQuestionContents', width: 250},
+            { Header: '미팅명', accessor: 'conferenceTitle', width: 180},
+            { Header: '그룹명', accessor: 'groupName', width: 140},
+            { Header: '질문 일자', accessor: 'conferenceQuestionDate', width: 220},
+        ], []
+    );
+
+    useEffect(() => {
+        axios({
+            method: 'GET',
+            url: `${API}/user/my-question-record?nowPage=1&items=10`, // nowPage와 items 변수로 넣어야됨. nowpage는 사용자가 2페이지를 놓으면 바껴야댐
+            headers: {
+                'Content-Type': 'application/json',
+                ACCESS_TOKEN: `${localStorage.getItem('ACCESS_TOKEN')}`,
+            },
+        })
+        .then((response) => {
+            console.log(response.data.question_record)
+            setQuestionRecord(response.data.question_record);
+        })
+    }, [API]);
+
     return(
-        <div>
-            <h1>여긴 마이 페이지 질문 리스트</h1>
-        </div>
+        <Scrollsize>
+            <Styles>
+                <CreateTable columns={columns} data={data} TableNavHandler={TableNavHandler} />
+            </Styles>
+        </Scrollsize>
     );
 };
 
 export default MyPageQuestionList;
+
+const Styles = styled.div`
+  text-align: center;
+  padding: 1rem;
+  table {
+    border-spacing: 0;
+    border: 1px solid black;
+    tr {
+      :last-child {
+        td {
+          border-bottom: 0;
+        }
+      }
+    }
+    th,
+    td {
+      margin: 0;
+      padding: 0.5rem;
+      border-bottom: 1px solid black;
+      border-right: 1px solid black;
+      :last-child {
+        border-right: 0;
+      }
+    }
+  }
+`;
+const Scrollsize = styled.div`
+  height: 46vh;
+  overflow-y: scroll;
+`;
